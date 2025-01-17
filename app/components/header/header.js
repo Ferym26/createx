0d8cbdfa7d742003351e12c5e47c3c302e14cc53
suvorova.a <suvorova.a@brainrocket.com>
2025-01-17 13:06:33 +0200

    fixed comments, added js toggle sidebar

---
+++ app/components/header/header.js
@@ -0,0 +1,16 @@
+let burgerBtn = document.getElementById("burgerBtn");
+let sidebar = document.getElementById("sidebarMob");
+
+function slideNav() {
+  burgerBtn.classList.toggle("active");
+  if (burgerBtn.classList.contains("active")) {
+    sidebar.style.transform = "translateX(250px)";
+  } else {
+    sidebar.style.transform = "translateX(-250px)";
+  }
+}
+
+function closeNav() {
+  burgerBtn.classList.remove("active");
+  sidebar.style.transform = "translateX(-250px)";
+}
