cadf6c7f5f2be436bd9c8e2f671bc0a673423bad
Hardzei Yauhen <hardzei.y@soft2bet.com>
2025-01-20 11:06:35 +0400

    js clear

--- app/js/app.js
+++ app/js/app.js
@@ -2,6 +2,8 @@ import Tabs from 'bootstrap/js/dist/tab.js';
 import Collapse from 'bootstrap/js/dist/collapse.js';
 import Modal from 'bootstrap/js/dist/modal.js';
 
+import { header } from '../components/header/header.js'
+
 document.addEventListener('DOMContentLoaded', () => {
-	// Custom JS
+	header();
 })
