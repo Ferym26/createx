23689091510980fdc47449277f7fbf98c10465c8
suvorova.a <suvorova.a@brainrocket.com>
2025-01-21 10:02:57 +0200

    test commit

---
+++ app/components/section-hero/section-hero.js
@@ -0,0 +1,52 @@
+import Swiper from "swiper";
+import { Navigation, Autoplay, Pagination } from "swiper/modules";
+
+export const hero = {
+  init() {
+    this.slider();
+  },
+
+  slider() {
+    const swiper = new Swiper(".js_hero-swiper", {
+      modules: [Navigation, Autoplay, Pagination],
+      loop: false,
+      slidesPerView: 1,
+      spaceBetween: 0,
+      watchOverflow: true,
+      watchSlidesVisibility: true,
+      autoplay: {
+        delay: 3000,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".js_hero-slider-next",
+        prevEl: ".js_hero-slider-prev",
+      },
+      breakpoints: {
+        // 1280: {
+        // 	slidesPerView: 3,
+        // 	spaceBetween: 0,
+        // },
+      },
+      on: {
+        // events
+        slideChange(instance) {
+          let activeIndex = instance.activeIndex;
+          const paginationArr = document.querySelectorAll(
+            ".slider__indicator-btn"
+          );
+          paginationArr.forEach((el) => {
+            el.classList.remove("active");
+          });
+          paginationArr[activeIndex].classList.add("active");
+        },
+      },
+    });
+    setTimeout(() => {
+      swiper.slideTo(3);
+    }, 3000);
+  },
+};
--- app/js/app.js
+++ app/js/app.js
@@ -1,11 +1,11 @@
-import Tabs from 'bootstrap/js/dist/tab.js';
-import Collapse from 'bootstrap/js/dist/collapse.js';
-import Modal from 'bootstrap/js/dist/modal.js';
+import Tabs from "bootstrap/js/dist/tab.js";
+import Collapse from "bootstrap/js/dist/collapse.js";
+import Modal from "bootstrap/js/dist/modal.js";
 
-import { header } from '../components/header/header.js'
-import { hero } from '../components/section-hero/script.js'
+import { header } from "../components/header/header.js";
+import { hero } from "../components/section-hero/section-hero.js";
 
-document.addEventListener('DOMContentLoaded', () => {
-	header();
-	hero.init();
-})
+document.addEventListener("DOMContentLoaded", () => {
+  header();
+  hero.init();
+});
