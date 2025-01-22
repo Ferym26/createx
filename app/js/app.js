e7d095a8d7c815acca84f63744ebd3ecba4b8fb1
suvorova.a <suvorova.a@brainrocket.com>
2025-01-22 13:19:59 +0200

    created iframe on click event

--- app/js/app.js
+++ app/js/app.js
@@ -4,8 +4,10 @@ import Modal from "bootstrap/js/dist/modal.js";
 
 import { header } from "../components/header/header.js";
 import { hero } from "../components/section-hero/section-hero.js";
+import { youtube } from "../components/section-promo/section-promo.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   header();
   hero.init();
+  youtube();
 });
