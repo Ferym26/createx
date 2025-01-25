300c9b5c5e32ab1dddc481b4b87c51c18255bc7a
suvorova.a <suvorova.a@brainrocket.com>
2025-01-25 18:35:22 +0200

    created dynamic cards in services section

--- app/js/app.js
+++ app/js/app.js
@@ -5,9 +5,12 @@ import Modal from "bootstrap/js/dist/modal.js";
 import { header } from "../components/header/header.js";
 import { hero } from "../components/section-hero/section-hero.js";
 import { youtube } from "../components/section-promo/section-promo.js";
+import { cards } from "../components/section-services/section-services.js";
+// import { cards } from "../blocks/card-services/card-services.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   header();
   hero.init();
   youtube();
+  cards();
 });
